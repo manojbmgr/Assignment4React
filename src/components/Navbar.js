b475382f4009a51663d0603acaf97b3bf74b2981
manojbmgr <43365915+manojbmgr@users.noreply.github.com>
2025-05-28 14:10:45 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ const AppNavbar = () => {
   return (
     <Navbar bg="primary" variant="dark">
       <Container className="d-flex justify-content-center">
-        <Navbar.Brand className="mx-auto">To-Do List</Navbar.Brand>
+        <Navbar.Brand className="mx-auto">To-Do List App Using localStorage</Navbar.Brand>
       </Container>
     </Navbar>
   );
